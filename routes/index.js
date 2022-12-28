0e395983045dc0d63ac11ea5b818069e895315fb
Nya-Risu <grivko.v.i.21@gmail.com>
2022-12-28 23:21:05 +0300

    4.2

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* Get home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+/* Страница Лоуренса */
+router.get('/Lawrence', function(req, res, next) {
+  res.send(<h1>Лоуренс</h1>);
+});
+
+/* Страница Холо */
+router.get('/Holo', function(req, res, next) {
+  res.send(<h1>Холо</h1>);
+});
+
+/* Страница Мьюри */
+router.get('/Myuuri', function(req, res, next) {
+  res.send(<h1>Мьюри</h1>);
 });
 
 module.exports = router;
